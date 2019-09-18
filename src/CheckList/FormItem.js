9f86b9b754524f182a298c280e19e93d216ebf1f
Trabajo <gabrielc1990poker@gmail.com>
2019-09-18 17:57:29 -0500

    FormItem

--- src/CheckList/FormItem.js
+++ src/CheckList/FormItem.js
@@ -1,13 +1,27 @@
 import React from 'react'
 
+
+function buttonsToShow(btnsToShow)
+{
+  let buttons = {
+    'create': <a key="create"> Crear </a>,
+    'clean': <a key="clean"> Clean </a>,
+    'edit': <a key="edit"> Edit </a>,
+    'guardar': <a key="guardar"> Guardar </a>,
+  }
+
+  return btnsToShow.map((value,index) => buttons[value] );
+}
+
+
 class FormItem extends React.Component
 {
   constructor(props){
     super(props)
-
     this.state.setState({
       description: this.props.description,
-      opened: this.props.opened,
+      isOpen: this.props.opened,
+      buttonsActions: this.props.buttonsActions
     })
   }
 
@@ -16,19 +30,23 @@ class FormItem extends React.Component
       <div className="form-item">
         <div className="input-ele">
           <input 
+          className="form-control"
           value={this.state.description} 
-          {this.state.isOpen ? 'readonly=readonly' : ''} />
+          {this.state.isOpen ? 'readonly=readonly' : ''} 
+          />
         </div>
 
         <div className="actions-button">
+          {buttonsToShow(this.state.buttonsActions)}
         </div>
 
       </div>
-    );
+    )
   }
 }
 
 FormItem.defaultProps = {
-  opened: true,
+  isOpen: true,
   description: '',
+  buttonsActions : ['create']
 }
--- src/CheckList/List.js
+++ src/CheckList/List.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Item from './Item';
+import FormItem from './FormItem';
 
 /**
  * Componente de prueba de un crud de elementos sencillos
@@ -11,32 +12,23 @@ class List extends React.Component {
 
   constructor(props){
     super(props)
-    this.state = { nombre: '' }
-
-    setInterval(() => {
-      let randomName = parseInt(Math.random() * 100000);
-      this.setState({ nombre : randomName })
-    }, 500);
-
+    this.state = { 
+      titleList: this.props.titleList,
+      openToCreated: this.props.openToCreated,
+      items: this.props.items,
+    }
   }
 
   render() {
 
     return (
       <div className="App-Listado">
-        <ul>
-
-          <Item
-            isChecked={true}
-            value={this.state.nombre}
-          />
-
-          <Item
-            value={this.state.nombre}
-            isChecked={false}
-          />
-
-        </ul>  
+        {/* Item de formulario principal para crear otros */}
+        {this.props.openToCreated ? <FormItem > ? ''}
+        
+        {/* Iteracion de items */}
+        {this.state.items.map((value, index) => <FormItem description={value.descripcion} >  )}}
+            
       </div>
     )
   }
